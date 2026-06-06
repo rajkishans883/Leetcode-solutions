0e3ae56d3a7c142bcac43bfd229842a3d8e23183
Raj Kishan <rajkishans883@gmail.com>
2026-06-07 01:50:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.53%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int totalPath(int m,int n,vector<vector<int>>&dp){
+            if(m<0 or n<0)return 0;
+            if(m==0 and n==0)return 1;
+            if(dp[m][n]!=-1)return dp[m][n];
+            int right=totalPath(m-1,n,dp);
+            int left=totalPath(m,n-1,dp);
+
+            return dp[m][n]=right+left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return totalPath(m-1,n-1,dp);
+    }
+};
