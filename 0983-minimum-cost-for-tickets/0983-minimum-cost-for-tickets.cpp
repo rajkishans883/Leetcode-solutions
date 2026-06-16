01d15811632927c41c13f2360dab3dc8ff604dd5
Raj Kishan <rajkishans883@gmail.com>
2026-06-16 22:44:42 +0530

    Time: 0 ms (100%), Space: 13.4 MB (54.67%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int dp[365];
-    int count_min(vector<int>&days,vector<int>& costs,int n, int i){
-        if(i>=n){
-            return 0;
-        }
-        if(dp[i]!=-1)return dp[i];
-
-
-        // one - day cost
-
-        int day_1= costs[0]+count_min(days,costs,n,i+1);
-
-        int j=i;
-        while(j<n and days[j]<days[i]+7){
-            j++;
-        }
-        int day_7=costs[1]+count_min(days,costs,n,j);
-        j=i;
-        while(j<n and days[j]<days[i]+30){
-            j++;
-        }
-        int day_30=costs[2]+count_min(days,costs,n,j);
-        return dp[i]=min(day_1,min(day_7,day_30));
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n= days.size();
-        memset(dp,-1,sizeof(dp));
-        return count_min(days,costs,n,0);
+        int n=days.size();
+        int last_day=days[n-1];
+        set<int>st;
+        vector<int>dp(last_day+1);
+        for(auto it:days){
+            st.insert(it);
+        }
+        dp[0]=0;
+        
+        for(int i=1;i<=last_day;i++){
+
+            if(st.find(i)==st.end()){
+                dp[i]=dp[i-1];
+                continue;
+            }
+            dp[i]=INT_MAX;
+            int day_1=costs[0]+dp[max(i-1,0)];
+            int day_7=costs[1]+dp[max(i-7,0)];
+            int day_30=costs[2]+dp[max(i-30,0)];
+
+            dp[i]=min(day_1,min(day_7,day_30));
+
+        }
+
+        return dp[last_day];
+
     }
 };
