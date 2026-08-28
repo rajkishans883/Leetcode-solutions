683261fe948f349cd10f18983a7532257a5e1f2a
Raj Kishan <rajkishans883@gmail.com>
2026-08-28 20:27:00 +0530

    Time: 0 ms (100%), Space: 16.1 MB (64.04%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -22,7 +22,19 @@ public:
     }
     int maxProfit( vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return profit_value(n,0,prices,dp,1);
+        vector<vector<int>>dp(n+2,vector<int>(2,-0));
+        
+        for(int i=n-1;i>=0;i--){
+            for(int buy=1;buy>=0;buy--){
+                if(buy){
+                    dp[i][buy]=max((-prices[i]+dp[i+1][0]),dp[i+1][1]);
+                }
+                else{
+                    dp[i][buy]=max((+prices[i]+dp[i+2][1]),dp[i+1][0]);
+            
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
